f663fb3c261e6b9a694a998d0ce159e1403339bd
drodol <drodol@fastmail.com>
2024-11-22 16:58:24 +0100

    changed the site url in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
-  site: 'https://drodol.com',  // Replace with your domain or GitHub Pages URL
+  site: 'https://drodol.github.io/drodolcom',  // Replace with your domain or GitHub Pages URL
   base: '/',
   integrations: [
     tailwind(),
